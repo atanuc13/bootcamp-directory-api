7819a5a7a49d79ccae3a6f88e38b63e8dc3017d0
Atanu Chatterjee <atanuc13@gmail.com>
2023-08-07 12:35:42 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async() => {
-    const conn = await mongoose.connect(process.env.DB_CONNECTION1, {
+    const conn = await mongoose.connect(process.env.DB_CONNECTION, {
         useNewUrlParser: true
     });
 
